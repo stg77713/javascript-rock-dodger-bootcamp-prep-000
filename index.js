16593315a7e8f43a366eaac4fbf2cb0b2f78c6a3
George Lugo <stg77713@gmail.com>
2020-03-15 21:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge.
                */
-        rockLeftEdge <=       
+        (rockLeftEdge <= dodgerLeftEdge)       
                ) {
       return true
     }
