312add4f8290f5dbd2e2daa12a27bb54de4b68c0
George Lugo <stg77713@gmail.com>
2020-03-15 22:57:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function createRock(x) {
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock(rock) {
+  function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
     /**
